33ed9f7e20d9b53909595841e89e54fbf707f512
Devendra <ingale.devendra20@gmail.com>
2023-10-06 13:13:09 +0530

    Second

--- src/main/java/com/example/travelbookingms/TravelbookingMsApplication.java
+++ src/main/java/com/example/travelbookingms/TravelbookingMsApplication.java
@@ -2,7 +2,9 @@ package com.example.travelbookingms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class TravelbookingMsApplication {
 
